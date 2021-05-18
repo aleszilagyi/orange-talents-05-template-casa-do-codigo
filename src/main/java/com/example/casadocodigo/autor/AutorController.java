df634a021a4cdc39fff0fd13ea5c57e64606cada
Alexandre Szilagyi Santos <alexandre.szilagyi.santoz@gmail.com>
2021-05-18 16:34:38 -0400

    Realizada a primeira tarefa de cadastrar novo Autor

--- src/main/java/com/example/casadocodigo/autor/AutorController.java
+++ src/main/java/com/example/casadocodigo/autor/AutorController.java
@@ -2,22 +2,27 @@ package com.example.casadocodigo.autor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/autores")
+@RequestMapping
 public class AutorController {
 
     @Autowired
     private AutorRepository autorRepository;
+    @Autowired
+    private EmailUnicoValidator emailUnicoValidator;
 
-    @PostMapping
+    @InitBinder
+    public void init(WebDataBinder binder){
+        binder.addValidators(emailUnicoValidator);
+    }
+
+    @PostMapping("/autores")
     @Transactional
     public ResponseEntity<AutorDto> cadastrar(@RequestBody @Valid FormDto form) {
         AutorEntidade autor = form.converter();
