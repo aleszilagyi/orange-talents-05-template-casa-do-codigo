a9989d5852fee6509e61947165e0be7bdf21720f
Alexandre Szilagyi Santos <alexandre.szilagyi.santoz@gmail.com>
2021-05-18 16:34:38 -0400

    Realizada a tarefa de e-mail único com o errorhandler

--- src/main/java/com/example/casadocodigo/autor/AutorEntidade.java
+++ src/main/java/com/example/casadocodigo/autor/AutorEntidade.java
@@ -6,16 +6,23 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Entity
 public class AutorEntidade {
 
     private @Id @GeneratedValue(strategy = GenerationType.IDENTITY) Long id;
-    private String nome;
-    private String email;
-    private String descricao;
+    private @NotBlank String nome;
+    private @NotBlank @Email String email;
+    private @NotBlank @Size(max = 400) String descricao;
     private @CreationTimestamp LocalDateTime momentoCriacao;
+
+    @Deprecated
+    public AutorEntidade() {
+    }
 
     public AutorEntidade(String nome, String email, String descricao) {
         this.nome = nome;
